3fa3bf210c8c2bed92a014d4bd1eede5eea38ffc
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-29 15:28:54 +0530

    daily insta news

--- Instagram_Bot/insta_news.py
+++ Instagram_Bot/insta_news.py
@@ -1,29 +1,52 @@
 
-import requests, random
+from instagrapi import Client
+import requests, random, os
+from PIL import Image
 
-news_api = input('\nEnter news_api Key : ')
+try: os.mkdir('images')
+except: pass
+
+news_api = input('\nEnter NewsAPI Key : ')
+passwd = input('\nEnter Instagram Password : ')
 
 source = ['bbc-news', 'cnn', 'the-verge', 'time', 'the-wall-street-journal']
 source = random.choice(source)
 
-print('\n', source)
 gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={news_api}'
 
 req = requests.get(gets)
 box = req.json()['articles']
 cap = []
 
+def make_square(im, min_size=256, fill_color=(255,255,255,0)):
+    img = Image.open(im)
+    x, y = img.size
+
+    size = max(min_size, x, y)
+    new_im = Image.new('RGB', (size, size), fill_color)
+
+    new_im = new_im.convert("RGB")
+    new_im.paste(img, (int((size - x) / 2), int((size - y) / 2)))
+    new_im.save(im)
+
 for j, i in enumerate(box):
-    if i['description'] == None:
-        i['description'] = 'Read More'
-
-    tweet = f'''
-({j+1}). {i['title']}
-
-{i['description']}
-
-{i['url']}
-'''
+    tweet = f'({j+1}). {i["title"]}\n'
     cap.append(tweet)
+    img = i['urlToImage']
+    r = requests.get(img, allow_redirects=True)
 
-print('\n'.join(cap))
+    path = f'images/{j}.jpg'
+    open(path, 'wb').write(r.content)
+    make_square(path)
+
+bot = Client()
+user = 'vix.bot'
+
+bot.login(username = user, password = passwd)
+album_path = ['images/'+i for i in os.listdir('images')]
+
+text = f'Read More:\n https://googleadsense.pythonanywhere.com/news/{source}\n\n'
+bot.album_upload(
+    album_path,
+    caption = text + '\n'.join(cap)
+)
