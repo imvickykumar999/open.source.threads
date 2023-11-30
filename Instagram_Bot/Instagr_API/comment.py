76b5ef9b70c5c0a258d2558f8c6f7ce6d6bbc44a
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-30 18:30:19 +0530

    Update comment.py

--- Instagram_Bot/Instagr_API/comment.py
+++ Instagram_Bot/Instagr_API/comment.py
@@ -26,22 +26,20 @@ media_id = cl.media_id(media_pk)
 comment = cl.media_comment(
     media_id, 
     f"MediaID = (PostID_UserID) : {media_id}"
-) # do comment
+)
 
-
-cl.comment_like(comment.pk) # like comment
-# cl.comment_unlike(comment.pk) # like comment
+cl.comment_like(comment.pk)
+# cl.comment_unlike(comment.pk)
 
 
 reply = cl.media_comment(
     media_id, 
     f"Replying Comment ID : {comment.pk}", 
     replied_to_comment_id=comment.pk
-) # do reply
+)
 
-
-cl.comment_like(reply.pk) # unlike reply
-# cl.comment_unlike(reply.pk) # unlike reply
+cl.comment_like(reply.pk)
+# cl.comment_unlike(reply.pk)
 
 
 (comments_part, next_min_id) = cl.media_comments_chunk(media_id, 100)
@@ -50,16 +48,23 @@ bulk_delete = []
 
 for i in comments_part:
     i = json.loads(i.json())
-    pprint(i['pk'], i['text']) # show comments
+    pprint(i['pk'], i['text'])
     bulk_delete.append(i['pk'])
 
 
-bulk_delete.pop(comment.pk) # don't delete comment
-bulk_delete.pop(reply.pk) # don't delete reply
-cl.comment_bulk_delete(media_id, bulk_delete) # delete appended
+bulk_delete.pop(comment.pk)
+bulk_delete.pop(reply.pk)
+cl.comment_bulk_delete(media_id, bulk_delete)
 
 
 '''
     raise RateLimitError(**last_json)
 instagrapi.exceptions.RateLimitError: Please wait a few minutes before you try again.
+
+API rate limit
+--------------
+
+Every third-party application connected to Instagram's API has a restriction 
+on how many times it may access its data. Instagram has recently decreased 
+its API restriction from 5,000 to 200 requests per hour.
 '''
