980696a05b4c5fef8e3287e0da6e698b544e03c8
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-30 16:14:49 +0530

    bot: like and comment

--- Instagram_Bot/Instagr_API/comment.py
+++ Instagram_Bot/Instagr_API/comment.py
@@ -5,7 +5,9 @@ from instagrapi import Client
 
 USERNAME = 'vix.bot'
 PASSWORD = input('\nEnter Instagram Password : ')
+
 pid = 'C0O3JPZJ3UN'
+# pid = input('Enter Post ID : ')
 
 cl = Client()
 cl.login(USERNAME, PASSWORD)
@@ -13,6 +15,21 @@ cl.login(USERNAME, PASSWORD)
 media_pk = cl.media_pk_from_url(f'https://www.instagram.com/p/{pid}/')
 media_id = cl.media_id(media_pk)
 
-comment = cl.media_comment(media_id, f"Media ID : {media_id}")
-cod = comment.dict()
-print(cod.keys())
+comment = cl.media_comment(
+    media_id, 
+    f"{media_id}"
+)
+cl.comment_like(comment.pk)
+
+print('\nComment Dictionary\n')
+print(comment.dict())
+
+reply = cl.media_comment(
+    media_id, 
+    f"{comment.dict()}", 
+    replied_to_comment_id=comment.pk
+)
+cl.comment_like(reply.pk)
+
+print('\nReply Dictionary\n')
+print(reply.dict())
